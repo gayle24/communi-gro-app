07ea4039f855cab90e2697edbb61b8855e97f071
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-08-04 09:12:48 +0300

    home page styling

--- communi-gro-app/src/Home.js
+++ communi-gro-app/src/Home.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 function Home() {
+
+    const backgroundStyle = {
+        backgroundImage: `url('https://f8n-production-collection-assets.imgix.net/0x987e51B6f520a5027290dBdBAA9Ccc1194beBDB4/10/nft.png?q=80&auto=format%2Ccompress&cs=srgb&h=640')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh', // Adjust the height as needed
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        textAlign: 'center',
+      };
+
   return (
 
-    <div className='home-page'>
+    <div style={backgroundStyle}>
       <h1>Welcome to CommuniGro</h1>
       <p>Discover a world of plants you can grow, indoors and outdoors.</p>
       <p>Order seedlings, share your experiences, and connect with fellow gardeners.</p>
-      
       {/* <Link to='/plantlist'>Get Started</Link> */}
     </div>
 
