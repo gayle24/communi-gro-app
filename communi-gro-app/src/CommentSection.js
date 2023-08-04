52529118216ec7439c6a55d00c81e15e482a77e1
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-08-04 08:42:34 +0300

    setting states

--- communi-gro-app/src/CommentSection.js
+++ communi-gro-app/src/CommentSection.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 function CommentSection() {
+    const [email, setEmail] = useState('');
+  const [comment, setComment] = useState('');
+  const [submittedComments, setSubmittedComments] = useState([]);
   return (
-    <div>CommentSection</div>
+    <div>
+        <form>
+
+        </form>
+    </div>
   )
 }
 
